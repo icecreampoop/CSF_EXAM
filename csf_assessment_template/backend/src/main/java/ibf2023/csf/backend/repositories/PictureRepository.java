7514c78d47d4185ef24fecc98faf70b7e26348fd
icecreampoop <pchiku@hotmail.sg>
2024-06-14 17:02:31 +0800

    pretty sure i need to configure mongotemplate but idk how

--- csf_assessment_template/backend/src/main/java/ibf2023/csf/backend/repositories/PictureRepository.java
+++ csf_assessment_template/backend/src/main/java/ibf2023/csf/backend/repositories/PictureRepository.java
@@ -4,22 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
+
 import ibf2023.csf.backend.models.PictureModel;
 
 @Repository
 public class PictureRepository {
 
-	private static String collectionName = "travelpics";
+	private static String dbName = "travelpics";
 
 	@Autowired
 	MongoTemplate template;
 
 	// TODO Task 4.2
-	// You may change the method signature by adding parameters and/or the  return type
-	// You may throw any exception 
-	// TODO IMPORTANT: Write the native mongo query in the comments above this method
+	// You may change the method signature by adding parameters and/or the return
+	// type
+	// You may throw any exception
+	// TODO IMPORTANT: Write the native mongo query in the comments above this
+	// method
 	public void save(PictureModel picture) {
-		template.save(picture, collectionName);
+		template.save(picture, dbName);
 	}
 
 }
